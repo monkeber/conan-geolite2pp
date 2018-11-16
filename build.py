294f66822206ce2a02883c1217815760a1099ea6
e.tiagniriadno <e.tiagniriadno@avantize.com>
2018-11-16 18:20:44 +0200

    Fixed build script

--- build.py
+++ build.py
@@ -3,6 +3,6 @@ from conan.packager import ConanMultiPackager
 
 if __name__ == "__main__":
     command = "conan remote add monkeber https://api.bintray.com/conan/monkeber/monkeber"
-    builder = ConanMultiPackager()
+    builder = ConanMultiPackager(docker_entry_script=command)
     builder.add_common_builds()
     builder.run()
