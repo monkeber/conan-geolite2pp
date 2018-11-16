d84e56d466558423e3b767b2740aad05bac9ee6b
e.tiagniriadno <e.tiagniriadno@avantize.com>
2018-11-16 18:42:30 +0200

    Fixed travis build

--- build.py
+++ build.py
@@ -5,5 +5,8 @@ if __name__ == "__main__":
     command = "conan remote add monkeber https://api.bintray.com/conan/monkeber/monkeber" \
         "&& sudo apt-get -qq update && sudo apt-get -qq install -y libmaxminddb-dev"
     builder = ConanMultiPackager(docker_entry_script=command)
-    builder.add_common_builds()
+    builder.add(settings={'compiler.libcxx': 'libstdc++11', 'arch': 'x86',
+        'build_arch': 'x86', 'build_type': 'Release', 'compiler': 'gcc'})
+    builder.add(settings={'compiler.libcxx': 'libstdc++11', 'arch': 'x86',
+        'build_arch': 'x86', 'build_type': 'Debug', 'compiler': 'gcc'})
     builder.run()
