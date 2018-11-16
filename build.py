33d29144a0e22c05676773a9b82a0a72ae168f06
e.tiagniriadno <e.tiagniriadno@avantize.com>
2018-11-16 18:56:45 +0200

    Changed build arch to x86_64

--- build.py
+++ build.py
@@ -5,8 +5,10 @@ if __name__ == "__main__":
     command = "conan remote add monkeber https://api.bintray.com/conan/monkeber/monkeber" \
         "&& sudo apt-get -qq update && sudo apt-get -qq install -y libmaxminddb-dev"
     builder = ConanMultiPackager(docker_entry_script=command)
-    builder.add(settings={'compiler.libcxx': 'libstdc++11', 'arch': 'x86',
-        'arch_build': 'x86', 'build_type': 'Release', 'compiler': 'gcc'})
-    builder.add(settings={'compiler.libcxx': 'libstdc++11', 'arch': 'x86',
-        'arch_build': 'x86', 'build_type': 'Debug', 'compiler': 'gcc'})
+
+    builder.add(settings={'compiler.libcxx': 'libstdc++11', 'arch': 'x86_64',
+        'arch_build': 'x86_64', 'build_type': 'Release', 'compiler': 'gcc'})
+    builder.add(settings={'compiler.libcxx': 'libstdc++11', 'arch': 'x86_64',
+        'arch_build': 'x86_64', 'build_type': 'Debug', 'compiler': 'gcc'})
+
     builder.run()
