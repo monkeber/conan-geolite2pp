09c5113b460c8d4f800bd9a5de0305f2a73c08d4
monkeber <27787571+monkeber@users.noreply.github.com>
2018-12-30 01:01:54 +0200

    Tried to fix build

--- conanfile.py
+++ conanfile.py
@@ -1,18 +1,20 @@
 from conans import ConanFile, CMake, tools
 import os
 
-
 class Geolite2Conan(ConanFile):
     name = "geolite2pp"
     version = "0.0.1-2561"
     url = "https://github.com/monkeber/conan-geolite2pp"
     description = "C++ API for MaxMind's GeoLite2 Database"
     settings = "os", "compiler", "build_type", "arch"
-    requires = "maxminddb/1.3.2@monkeber/stable"
     generators = "cmake"
     build_policy = "missing"
-    options = { "shared": [True, False] }
-    default_options = "shared=False"
+
+    def requirements(self):
+        self.requires("maxminddb/1.3.2@monkeber/testing", private=False, override=False)
+
+    def imports(self):
+        self.copy("*", "include", "include", root_package="maxminddb")
 
     def source(self):
         tools.download("https://www.ccoderun.ca/GeoLite2PP/download" \
@@ -22,29 +24,30 @@ class Geolite2Conan(ConanFile):
         self.run("mv geolite2++-{}-Source geolite2++".format(self.version))
 
     def build(self):
-        pack_names = []
-        if tools.os_info.with_apt and self.settings.build_type == "Debug":
-            pack_names = ["libmaxminddb-dev"]
+        cxxflags = os.environ.get("CXXFLAGS", "")
+        if cxxflags:
+            cxxflags += " "
 
-        if pack_names:
-            installer = tools.SystemPackageTool()
-            installer.install(" ".join(pack_names))
+        print("---------------------------------------------")
+        cxxflags += "-isystem\\ {}".format(self.deps_cpp_info["maxminddb"].includedirs[0])
+        print(cxxflags)
+        self.run("dir " + self.deps_cpp_info["maxminddb"].includedirs[0])
 
         cmake = CMake(self)
-        cmake.configure(source_folder="geolite2++")
+        cmake.configure(source_folder="geolite2++", args=["-DCMAKE_CXX_FLAGS={}".format(cxxflags)])
         cmake.build()
 
     def package(self):
         self.run("cd geolite2++/scripts && ./geolite2pp_get_database.sh")
         self.copy("*.mmdb", dst="bin", src="geolite2++/scripts")
-        self.copy("*.h*", dst="include", src="geolite2++/src-lib")
+        self.copy("*.hpp", dst="include", src="geolite2++/src-lib")
+        self.copy("*.h", dst="include", src="geolite2++/src-lib")
         self.copy("*geolite2++.lib", dst="lib", keep_path=False)
         self.copy("*.a", dst="lib", keep_path=False)
-        if self.options.shared:
-            self.copy("*.dll", dst="bin", keep_path=False)
-            self.copy("*.so", dst="lib", keep_path=False)
-            self.copy("*.so.0", dst="lib", keep_path=False)
-            self.copy("*.dylib", dst="lib", keep_path=False)
+        self.copy("*.dll", dst="bin", keep_path=False)
+        self.copy("*.so", dst="lib", keep_path=False)
+        self.copy("*.so.0", dst="lib", keep_path=False)
+        self.copy("*.dylib", dst="lib", keep_path=False)
 
     def package_info(self):
         self.cpp_info.libs = ["geolite2++", "maxminddb"]
@@ -52,4 +55,3 @@ class Geolite2Conan(ConanFile):
         self.cpp_info.includedirs = ["include"]
         if self.settings.os == "Linux":
             self.cpp_info.libs.append("pthread")
-
